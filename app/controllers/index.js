fa1cb7843b14eb13e7387c4fcb09d74980c4869f
Thomas Ngo <tngo0508@gmail.com>
2018-04-22 17:08:17 -0700

    finish lesson 2

---
+++ app/controllers/index.js
@@ -0,0 +1,20 @@
+import Controller from '@ember/controller';
+import { match, not } from '@ember/object/computed';
+
+export default Controller.extend({
+
+  emailAddress: '',
+
+  isValid: match('emailAddress', /^.+@.+\..+$/),
+  isDisabled: not('isValid'),
+
+  actions: {
+
+    saveInvitation() {
+      alert(`Saving of the following email address is in progress: ${this.get('emailAddress')}`);
+      this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
+      this.set('emailAddress', '');
+    }
+  }
+
+});

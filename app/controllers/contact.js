e63be8ff4ee528c471ae4770100b46bc9142cdd0
Thomas Ngo <tngo0508@gmail.com>
2018-04-25 13:21:27 -0700

    "send message" button only active when message present and valid email has been entered

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -1,5 +1,7 @@
 import Controller from '@ember/controller';
 import { match, not } from '@ember/object/computed';
+import { gte } from '@ember/object/computed';
+import { and } from '@ember/object/computed';
 
 export default Controller.extend({
   headerMessage: 'Contact Page',
@@ -7,13 +9,15 @@ export default Controller.extend({
   emailAddress: '',
 
   isValid: match('emailAddress', /^.+@.+\..+$/),
-  isDisabled: not('isValid'),
+  isLongEnough: gte("message.length", 5),
+  isBothTrue: and('isValid', 'isLongEnough'),
+  isDisabled: not('isBothTrue'),
 
   actions: {
 
     saveInvitation() {
       alert(`Saving of the following email address and message is in progress: ${this.get('emailAddress')}, ${this.get('message')}`);
-      this.set('responseMessage', `Thank you! We've just saved your email address and message: ${this.get('emailAddress')}`);
+      this.set('responseMessage', `Thank you! We've just saved your email address and message: ${this.get('emailAddress')}, ${this.get('message')}`);
       this.set('emailAddress', '');
       this.set('message', '');
     }

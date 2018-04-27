e69aa0d5c811972954dc2cbfd91dd84bf70f1aef
Thomas Ngo <tngo0508@gmail.com>
2018-04-26 23:36:07 -0700

    add delete buttons to the lists on your Admin pages

--- app/routes/admin/contacts.js
+++ app/routes/admin/contacts.js
@@ -3,5 +3,16 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   model() {
     return this.store.findAll('contact');
+  },
+
+  actions: {
+
+    deleteContact(contact) {
+      let confirmation = confirm('Are you sure?');
+
+      if (confirmation) {
+        contact.destroyRecord();
+      }
+    }
   }
 })
--- app/routes/admin/invitations.js
+++ app/routes/admin/invitations.js
@@ -3,5 +3,16 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   model() {
     return this.store.findAll('invitation');
+  },
+
+  actions: {
+
+    deleteInvitation(invitation) {
+      let confirmation = confirm('Are you sure?');
+
+      if (confirmation) {
+        invitation.destroyRecord();
+      }
+    }
   }
 });
